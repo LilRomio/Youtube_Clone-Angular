5174ba36142a4918aee8c89076a257495b326bed
LilRomio <popescu.romeo9@gmail.com>
2022-12-29 22:02:06 +0200

    added ngfor in thumbnail - not working properly

--- src/app/components/main/thumbnail/thumbnail.component.ts
+++ src/app/components/main/thumbnail/thumbnail.component.ts
@@ -3,13 +3,68 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-thumbnail',
   templateUrl: './thumbnail.component.html',
-  styleUrls: ['./thumbnail.component.scss']
+  styleUrls: ['./thumbnail.component.scss'],
 })
 export class ThumbnailComponent implements OnInit {
+  data = [
+    {
+      video: 'src="../../../../assets/images/thumbnail1.png"',
+      icon: 'src="../../../../assets/images/Jack.png"',
+      title: 'Best Channel to learn coding',
+      subtitle: 'Easy Tuturials',
+      views: '15k views 2 days',
+    },
+    {
+      video: 'src="../../../../assets/images/thumbnail2.png"',
+      icon: 'src="../../../../assets/images/Jack.png"',
+      title: 'Best Channel to learn coding',
+      subtitle: 'Easy Tuturials',
+      views: '15k views 2 days',
+    },
+    {
+      video: 'src="../../../../assets/images/thumbnail3.png"',
+      icon: 'src="../../../../assets/images/Jack.png"',
+      title: 'Best Channel to learn coding',
+      subtitle: 'Easy Tuturials',
+      views: '15k views 2 days',
+    },
+    {
+      video: 'src="../../../../assets/images/thumbnail4.png"',
+      icon: 'src="../../../../assets/images/Jack.png"',
+      title: 'Best Channel to learn coding',
+      subtitle: 'Easy Tuturials',
+      views: '15k views 2 days',
+    },
+    {
+      video: 'src="../../../../assets/images/thumbnail5.png"',
+      icon: 'src="../../../../assets/images/Jack.png"',
+      title: 'Best Channel to learn coding',
+      subtitle: 'Easy Tuturials',
+      views: '15k views 2 days',
+    },
+    {
+      video: 'src="../../../../assets/images/thumbnail6.png"',
+      icon: 'src="../../../../assets/images/Jack.png"',
+      title: 'Best Channel to learn coding',
+      subtitle: 'Easy Tuturials',
+      views: '15k views 2 days',
+    },
+    {
+      video: 'src="../../../../assets/images/thumbnail7.png"',
+      icon: 'src="../../../../assets/images/Jack.png"',
+      title: 'Best Channel to learn coding',
+      subtitle: 'Easy Tuturials',
+      views: '15k views 2 days',
+    },
+    {
+      video: 'src="../../../../assets/images/thumbnail8.png"',
+      icon: 'src="../../../../assets/images/Jack.png"',
+      title: 'Best Channel to learn coding',
+      subtitle: 'Easy Tuturials',
+      views: '15k views 2 days',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
